7b0f0967af0eb23fc1882d5bed319f0bb70b0c22
héctor <dreamallica@gmail.com>
2022-08-09 10:56:09 -0300

    cambios

--- index.js
+++ index.js
@@ -1,18 +1,29 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const dotenv = require('dotenv');
 
 app.use(express.static('public'));
 
+//Defino el motor de plantillas a utilizar
+app.set('view engine', 'ejs');
+//Defino la localización de mis vistas
+app.set('views', path.join(__dirname, 'views'));
+
+app.get('/test', (req, res) => {
+    console.log('holas');
+    res.render('pagina');
+});
+
 app.get('/', (req, res) => {
+    res.render('pagina');
     //Conexión al cloud de Mongodb Atlas ...
     mongoose
         .connect('mongodb+srv://hrgarcia:EaFhXeNfxbG277Zz@cluster0.fs8tm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
             useNewUrlParser: true,
         })
         .then((con) => {
-            //console.log(con.connections);
-            res.sendFile('index.html', { root: path.join(__dirname, 'public') });
+            res.render('pagina');
         });
 });
 
