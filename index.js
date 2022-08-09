d924f010442f2115ec6a70a2c5e3b9932695494c
héctor <dreamallica@gmail.com>
2022-08-09 11:15:24 -0300

    test

--- index.js
+++ index.js
@@ -1,32 +1,36 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-const dotenv = require('dotenv');
+const mongoose = require('mongoose');
 
-app.use(express.static('public'));
+//Configurando archivos estáticos
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
 
 //Defino el motor de plantillas a utilizar
 app.set('view engine', 'ejs');
 //Defino la localización de mis vistas
 app.set('views', path.join(__dirname, 'views'));
-
-app.get('/test', (req, res) => {
-    console.log('holas');
-    res.render('pagina');
+let port = 3000;
+app.listen(port, () => {
+    console.log(`Servidor corriendo en el puerto ${port} correctamente`);
 });
 
-app.get('/', (req, res) => {
-    res.render('pagina');
+app.get('/test', (req, res) => {
     //Conexión al cloud de Mongodb Atlas ...
     mongoose
         .connect('mongodb+srv://hrgarcia:EaFhXeNfxbG277Zz@cluster0.fs8tm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
             useNewUrlParser: true,
         })
         .then((con) => {
-            res.render('pagina');
+            console.log('Conectado a la DB');
+            res.render('pagina2');
         });
 });
 
-app.listen(process.env.PORT || 3000);
+app.get('/', (req, res) => {
+    console.log('Dominio principal');
+    res.render('pagina');
+});
 
 module.exports = app;
