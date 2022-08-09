3ee5bede3238cad8ade194cd9ac08fca2ee65735
héctor <dreamallica@gmail.com>
2022-08-09 10:10:53 -0300

    Conexión DB

--- index.js
+++ index.js
@@ -1,12 +1,27 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const dotenv = require('dotenv');
 
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
     res.sendFile('index.html', { root: path.join(__dirname, 'public') });
 });
+
+//Carga de variables de entorno
+dotenv.config({ path: './config.env' });
+const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
+
+//Conexión al cloud de Mongodb Atlas ...
+mongoose
+    .connect(DB, {
+        useNewUrlParser: true,
+    })
+    .then((con) => {
+        //console.log(con.connections);
+        console.log('Connected to database');
+    });
 
 app.listen(process.env.PORT || 3000);
 
