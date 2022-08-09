ec5f6d43126eb3a4a5c66a949b2a998046309fa1
héctor <dreamallica@gmail.com>
2022-08-09 10:16:37 -0300

    mongoose

--- index.js
+++ index.js
@@ -6,22 +6,21 @@ const dotenv = require('dotenv');
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-    res.sendFile('index.html', { root: path.join(__dirname, 'public') });
+    //Conexión al cloud de Mongodb Atlas ...
+    mongoose
+        .connect(DB, {
+            useNewUrlParser: true,
+        })
+        .then((con) => {
+            //console.log(con.connections);
+            alert('Connected to database!!!');
+            res.sendFile('index.html', { root: path.join(__dirname, 'public') });
+        });
 });
 
 //Carga de variables de entorno
 dotenv.config({ path: './config.env' });
 const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
-
-//Conexión al cloud de Mongodb Atlas ...
-mongoose
-    .connect(DB, {
-        useNewUrlParser: true,
-    })
-    .then((con) => {
-        //console.log(con.connections);
-        console.log('Connected to database');
-    });
 
 app.listen(process.env.PORT || 3000);
 
